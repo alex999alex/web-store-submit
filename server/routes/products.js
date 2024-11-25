58a018a2a650183ffc7df46aa7b450e0a8e820c2
alex999alex <y3d7m5@gmail.com>
2024-11-24 22:06:35 -0400

    8

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,32 +3,19 @@ import multer from 'multer';
 import pkg from '@prisma/client';
 
 const { PrismaClient } = pkg;
-// const prisma = new PrismaClient();
-// import { PrismaClient } from '@prisma/client';
+
 import fs from 'fs';
 
 const router = express.Router();
 
-// Multer setup
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'public/api/images/'); // save uploaded files in `public/api/images` folder
-//     },
-//     filename: function (req, file, cb) {
-//         const ext = file.originalname.split('.').pop(); // get file extension
-//         const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext; // generate unique filename - current timestamp + random number between 0 and 1000.
-//         cb(null, uniqueFilename);
-//     }
-// });
-// const upload = multer({ storage: storage });
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         const uploadPath = 'public/api/images/';
-        cb(null, uploadPath); // Ensure this matches your existing directory
+        cb(null, uploadPath); 
     },
     filename: function (req, file, cb) {
-        const ext = file.originalname.split('.').pop(); // Get file extension
+        const ext = file.originalname.split('.').pop(); 
         const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext;
         cb(null, uniqueFilename);
     }
@@ -36,23 +23,23 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 
-// Prisma setup
+
 const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
 });
 
-// Get all products
+
 router.get('/all', async (req, res) => {
     const products = await prisma.product.findMany();
-    // res.send('All products');
+    
     res.json(products);
 });
 
-// Get a product by id
+
 router.get('/get/:id', async (req, res) => {
     const id = req.params.id;
   
-    // Validate id
+   
     const idRegex = /^\d+$/;
     if (!idRegex.test(id)) {
       return res.status(400).send('Invalid id format. Please provide an integer.');
@@ -71,18 +58,18 @@ router.get('/get/:id', async (req, res) => {
     }
 });
 
-// Add a new product
+
 router.post('/create', upload.single('image'), async (req, res) => {
     const { name, description, cost } = req.body;
     const filename = req.file ? req.file.filename : null;
 
-    // Validate input
+    
     if (!name || !description || !cost) {
         res.status(400).send('All fields are required.');
         return;
     }
 
-    // Convert cost to a float
+   
     const parsedCost = parseFloat(cost);
     if (isNaN(parsedCost)) {
         res.status(400).send('Invalid cost value.');
@@ -93,7 +80,7 @@ router.post('/create', upload.single('image'), async (req, res) => {
         data: {
             name: name,
             description: description,
-            cost: parsedCost,  // Use parsed cost here
+            cost: parsedCost,  
             filename: filename
         }
     });
@@ -102,40 +89,38 @@ router.post('/create', upload.single('image'), async (req, res) => {
 });
 
 
-    // Update a product by id
+   
     router.put('/update/:id', upload.single('image'), async(req, res) => {
 
-        // capture the inputs
+      
         const id = req.params.id;
         const { name, description, cost } = req.body;
-        // const { name, description, type, cost } = req.body;
+        
         const newFilename = req.file ? req.file.filename : null;
 
-        // validate the id
+        
         if (isNaN(id)) {
             return res.status(400).json({ message: 'Invalid id.' });
         }
 
-        // validate required fields
+       
         if (!name || !description || !type || !cost) {
             return res.status(400).json({ message: 'First name, last name, type, cost are required.' });
         }
 
-        // to-do: add additional validations (optional)
 
-        // Find the product by id (if not found, return 404)
         const product = await prisma.product.findUnique({
             where: {
             id: parseInt(id)
             }
         });
         
-        // If product not found, return 404
+     
         if (product === null) {
             return res.status(404).json({ message: 'product not found.' });
         }
         
-        // Delete old file if new file uploaded.
+
         if (newFilename && product.filename) {
             fs.unlink(`public/api/images/${product.filename}`, (err) => {
             if (err) {
@@ -144,7 +129,7 @@ router.post('/create', upload.single('image'), async (req, res) => {
             });
         }
 
-        // Update the database record
+ 
         const updatedproduct = await prisma.product.update({
             where: {
             id: parseInt(id)
@@ -152,37 +137,37 @@ router.post('/create', upload.single('image'), async (req, res) => {
             data: {
             name: name,
             description: description,
-            // type: type,
+   
             cost: cost || null,
-            filename: newFilename || product.filename // save the new filename or old filename if no new file uploaded
+            filename: newFilename || product.filename 
             }
         });
         
         res.json(updatedproduct);
     });
 
-// Delete a product by id
+
 router.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
 
-    // verify id is a number
+ 
     if (isNaN(id)) {
         return res.status(400).json({ message: 'Invalid id.' });
     }
 
-    // Find the product by id (if not found, return 404)
+
     const product = await prisma.product.findUnique({
         where: {
             id: parseInt(id)
         }
     });
 
-    // If product not found, return 404
+  
     if (product === null) {
         return res.status(404).json({ message: 'product not found.' });
     }
 
-    // delete the file (if product has one)
+
     if (product.filename) {
         fs.unlink(`public/api/images/${product.filename}`, (err) => {
             if (err) {
@@ -191,7 +176,7 @@ router.delete('/delete/:id', async (req, res) => {
         });
     }
 
-    // delete the record with prisma
+
     const deletedproduct = await prisma.product.delete({
         where: {
             id: parseInt(id)
@@ -202,46 +187,6 @@ router.delete('/delete/:id', async (req, res) => {
 });
 
 
-// // Purchase a product
-// router.post('/purchase', async (req, res) => {
-//     const { productId, quantity } = req.body;
-
-//     // Validate input
-//     if (!productId || !quantity || isNaN(quantity)) {
-//         return res.status(400).json({ message: 'All fields (productId, quantity) are required.' });
-//     }
-
-//     // Ensure the user is logged in
-//     const userId = req.session.user_id;
-//     if (!userId) {
-//         return res.status(401).json({ message: 'Unauthorized. Please log in first.' });
-//     }
-
-//     // Find the product by ID
-//     const product = await prisma.product.findUnique({
-//         where: { id: parseInt(productId) },
-//     });
-
-//     if (!product) {
-//         return res.status(404).json({ message: 'Product not found.' });
-//     }
-
-//     // Calculate total cost based on quantity
-//     const totalCost = (parseFloat(product.cost) * quantity).toFixed(2);
-
-//     // Create the purchase record
-//     const purchase = await prisma.purchase.create({
-//         data: {
-//             userId: userId,
-//             productId: parseInt(productId),
-//             quantity: parseInt(quantity),
-//             totalCost: totalCost,
-//         },
-//     });
-
-//     // Send a response with purchase details
-//     res.json({ message: 'Purchase successful', purchase });
-// });
 
 router.post('/purchase', async (req, res) => {
     const {
@@ -259,21 +204,21 @@ router.post('/purchase', async (req, res) => {
         invoice_total
     } = req.body;
 
-    // Check if the user is logged in
+ 
     const userId = req.session.user_id;
     if (!userId) {
         return res.status(401).json({ message: 'Unauthorized. Please log in first.' });
     }
 
-    // Validate input
+
     if (!street || !city || !province || !country || !postal_code || !credit_card || !credit_expire || !credit_cvv || !cart || !invoice_amt || !invoice_tax || !invoice_total) {
         return res.status(400).json({ message: 'All fields are required.' });
     }
 
     try {
-        // Start a transaction to ensure data consistency
+
         const purchase = await prisma.$transaction(async (prisma) => {
-            // Create the Purchase record
+
             const newPurchase = await prisma.purchase.create({
                 data: {
                     customer_id: userId,
@@ -294,14 +239,14 @@ router.post('/purchase', async (req, res) => {
 
             const purchase_id = newPurchase.purchase_id;
 
-            // Process cart
+
             const cartItems = cart.split(',').map(Number);
             const productCounts = {};
             for (const productId of cartItems) {
                 productCounts[productId] = (productCounts[productId] || 0) + 1;
             }
 
-            // Create PurchaseItem records
+
             const purchaseItems = [];
             for (const [productId, quantity] of Object.entries(productCounts)) {
                 purchaseItems.push({
--- server/server.js
+++ server/server.js
@@ -8,31 +8,31 @@ import productsRouter from './routes/products.js';
 const port = process.env.PORT || 3000;
 const app = express();
 
-// middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// cors middleware
+
 app.use(cors({
-  origin: 'http://localhost:5173', // react client
-  credentials: true // allow cookies
+  origin: 'http://localhost:5173', 
+  credentials: true 
 }));
 
-// express-session middleware
+
 app.use(session({
   secret: 'fkldjbnfdkFTFT5efd3$$sdg89F',
   resave: false,
   saveUninitialized: true,
   cookie: { 
     httpOnly: true,
-    secure: false,  // Set to `true` if using HTTPS in production
-    sameSite: 'lax',  // Consider 'none' if client and server are on different origins
-    maxAge: 3600000 // 1 hour in milliseconds
+    secure: false,  
+    sameSite: 'lax',  
+    maxAge: 3600000 
   }
 }));
 
-// routes
+
 app.use('/api/', homeRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/products', productsRouter);
@@ -40,5 +40,3 @@ app.use('/api/products', productsRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-//1
